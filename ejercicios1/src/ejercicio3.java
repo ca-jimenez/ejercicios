7fcd6bceab2c92c9e18a7ffc0f8ad601b1eafa63
ca-jimenez <carolina.jimenez@mercadolibre.com>
2021-03-22 11:10:33 -0300

    ejercicio 3

---
+++ ejercicios1/src/ejercicio3.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class ejercicio3 {
+
+    public static void main(String args[]) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Ingrese n");
+
+        int n = scanner.nextInt();
+
+        boolean esPrimo = true;
+
+        for (int i = 2; i < n; i++) {
+
+            if (n % i == 0) {
+                esPrimo = false;
+                break;
+            }
+        }
+
+        if (n < 2) {
+            esPrimo = false;
+        }
+
+        if (esPrimo) {
+            System.out.println(n + " es primo");
+
+        } else {
+            System.out.println(n + " no es primo");
+        }
+
+        scanner.close();
+    }
+}
