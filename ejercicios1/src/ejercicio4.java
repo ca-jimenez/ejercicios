d3da565845ea569d951a21e0e9412950e2a3e256
ca-jimenez <carolina.jimenez@mercadolibre.com>
2021-03-22 11:10:41 -0300

    ejercicio 4

---
+++ ejercicios1/src/ejercicio4.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class ejercicio4 {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Ingrese n");
+
+        int n = scanner.nextInt();
+
+        int primos = 0;
+
+        for (int i = 2; primos < n; i++) {
+            boolean esPrimo = true;
+
+            for (int j = 2; j < i; j++) {
+
+                if (i % j == 0) {
+                    esPrimo = false;
+                    break;
+                }
+            }
+
+            if (esPrimo) {
+                primos++;
+                System.out.println(i);
+            }
+        }
+
+        scanner.close();
+    }
+}
