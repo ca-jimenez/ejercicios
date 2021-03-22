99a259dfe7d7ee435bb7ffccc9c5da4d3d75b9dd
ca-jimenez <carolina.jimenez@mercadolibre.com>
2021-03-22 16:56:48 -0300

    Create Fraccion.java

---
+++ ejercicios2/src/Fraccion.java
@@ -0,0 +1,34 @@
+public class Fraccion {
+
+    public static double sumar(double f1, double f2) {
+        return f1 + f2;
+    }
+
+    public static double sumar(double f, int n) {
+        return f + n;
+    }
+
+    public static double restar(double f1, double f2) {
+        return f1 - f2;
+    }
+
+    public static double restar(double f, int n) {
+        return f - n;
+    }
+
+    public static double multiplicar(double f1, double f2) {
+        return f1 * f2;
+    }
+
+    public static double multiplicar(double f, int n) {
+        return f * n;
+    }
+
+    public static double dividir(double f1, double f2) {
+        return f1 / f2;
+    }
+
+    public static double dividir(double f, int n) {
+        return f / n;
+    }
+}
