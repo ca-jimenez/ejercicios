25d70ef2dbc3a07559abd74b75426bfca6db29e4
ca-jimenez <carolina.jimenez@mercadolibre.com>
2021-03-22 16:38:08 -0300

    Create Libro.java

---
+++ ejercicios2/src/Libro.java
@@ -0,0 +1,53 @@
+public class Libro {
+
+    String title;
+    String isbn;
+    String author;
+
+    public Libro(String title, String isbn, String author) {
+        this.title = title;
+        this.isbn = isbn;
+        this.author = author;
+    }
+
+    public Libro() {
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public void prestamo() {
+
+    }
+
+    public void devolucion() {
+
+    }
+
+    @Override
+    public String tostring() {
+
+        return this.title + ", ISBN: " + this.isbn + ", author: " + author;
+    }
+}
