537c6908ec1c2a7aa2cf752c3812fc4311f49beb
ca-jimenez <carolina.jimenez@mercadolibre.com>
2021-03-22 16:21:10 -0300

    Create Contador.java

---
+++ ejercicios2/src/Contador.java
@@ -0,0 +1,31 @@
+public class Contador {
+
+    int value;
+
+    public Contador(int value) {
+        this.value = value;
+    }
+
+    public Contador() {
+    }
+
+    public Contador(Contador counter) {
+        this.value = counter.getValue();
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    public void addOne() {
+        this.value++;
+    }
+
+    public void substractOne() {
+        this.value--;
+    }
+}
