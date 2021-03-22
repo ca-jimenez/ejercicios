e0fdbb0460db4d8a1229432f43e643fd77b730d0
ca-jimenez <carolina.jimenez@mercadolibre.com>
2021-03-22 11:10:02 -0300

    ejercicio 1

---
+++ ejercicios1/src/ejercicio1.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class ejercicio1 {
+
+    public static void main(String args[]) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Ingrese un número");
+
+        int n = scanner.nextInt();
+
+        int evenN = 0;
+
+        for (int i = 0; evenN < n; i++) {
+
+            if (i % 2 == 0) {
+                System.out.println(i);
+                evenN++;
+            }
+        }
+
+        scanner.close();
+    }
+}
