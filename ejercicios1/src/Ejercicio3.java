a1aad29d68dbc0796fce76ec1b6bdf1c298bbeed
ca-jimenez <carolina.jimenez@mercadolibre.com>
2021-03-22 16:20:53 -0300

    file refactoring

--- ejercicios1/src/Ejercicio3.java
+++ ejercicios1/src/Ejercicio3.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class ejercicio3 {
+public class Ejercicio3 {
 
     public static void main(String args[]) {
 
--- ejercicios1/src/Ejercicio4.java
+++ ejercicios1/src/Ejercicio4.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class ejercicio4 {
+public class Ejercicio4 {
 
     public static void main(String[] args) {
 
