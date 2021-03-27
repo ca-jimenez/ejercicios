812927497b8339f411f12369b3f724b8535a4e6e
ca-jimenez <carolina.jimenez@mercadolibre.com>
2021-03-27 18:03:03 -0300

    dakar

---
+++ ejerciciosFinalesJava/src/Dakar/Auto.java
@@ -0,0 +1,13 @@
+package Dakar;
+
+public class Auto extends Vehiculo {
+
+    public Auto(int velocidad, int aceleracion, int anguloDeGiro, String patente){
+        this.setVelocidad(velocidad);
+        this.setAceleracion(aceleracion);
+        this.setPatente(patente);
+        this.setAnguloDeGiro(anguloDeGiro);
+        this.setPeso(1000);
+        this.setRuedas(4);
+    }
+}
---
+++ ejerciciosFinalesJava/src/Dakar/Vehiculo.java
@@ -0,0 +1,68 @@
+package Dakar;
+
+public abstract class Vehiculo {
+
+    private int velocidad;
+    private int aceleracion;
+    private int anguloDeGiro;
+    private String patente;
+    private float peso;
+    private int ruedas;
+
+    public int getVelocidad() {
+        return velocidad;
+    }
+
+    public void setVelocidad(int velocidad) {
+        this.velocidad = velocidad;
+    }
+
+    public int getAceleracion() {
+        return aceleracion;
+    }
+
+    public void setAceleracion(int aceleracion) {
+        this.aceleracion = aceleracion;
+    }
+
+    public int getAnguloDeGiro() {
+        return anguloDeGiro;
+    }
+
+    public void setAnguloDeGiro(int anguloDeGiro) {
+        this.anguloDeGiro = anguloDeGiro;
+    }
+
+    public String getPatente() {
+        return patente;
+    }
+
+    public void setPatente(String patente) {
+        this.patente = patente;
+    }
+
+    public float getPeso() {
+        return peso;
+    }
+
+    public void setPeso(int peso) {
+        this.peso = peso;
+    }
+
+    public int getRuedas() {
+        return ruedas;
+    }
+
+    public void setRuedas(int ruedas) {
+        this.ruedas = ruedas;
+    }
+
+    @Override
+    public boolean equals(Object vehiculo) {
+
+        Vehiculo thisVehicle = (Vehiculo) vehiculo;
+
+        return this.getPatente().equals(thisVehicle.getPatente());
+    }
+
+}
